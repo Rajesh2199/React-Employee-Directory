2f3cd4c2ea2ad61edf81032c593cba65ada90092
baduraje@isu.edu <baduraje@isu.edu>
2020-10-08 03:42:59 -0600

    added bootstrap style to Welcome component

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,14 @@
 import React, {Component } from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Welcome extends Component {
     render (){
         return (
-            <div className ="Welcome">
-                <header>Welcome to the User Directory.</header>
+            
+            <div>
+                <div className = "jumbotron bg-info text-white"> Welcome to the User Directory.</div>
+                     <h1 className ="lead">Here is the list of the current employees.</h1>
+               
             </div>
         )
     }
